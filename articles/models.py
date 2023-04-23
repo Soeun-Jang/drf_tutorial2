c9398260178dcde076a75e8a22f5ba546e43773a
Soeun_Jang <azulcielo_@naver.com>
2023-04-24 06:19:01 +0900

    Comment View함수, Serializer 추가

--- articles/models.py
+++ articles/models.py
@@ -9,3 +9,17 @@ class Article(models.Model):
   image = models.ImageField(blank=True, upload_to='%Y/%m/%d/')
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return str(self.title)
+  
+  
+class Comment(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comment_set")
+  content = models.TextField()
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+  
+  def __str__(self):
+    return str(self.content)
--- articles/serializers.py
+++ articles/serializers.py
@@ -1,20 +1,32 @@
 from rest_framework import serializers
-from articles.models import Article
+from articles.models import Article,Comment
 
 
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+      model = Comment
+      fields = '__all__'
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+      model = Comment
+      fields = ("content",)
 
   
 class ArticleSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    #obj 인스턴스의 user속성에서 email값 추출하여 반환해서 user필드에 추가
+    def get_user(self,obj):
+      return obj.user.email
     class Meta:
         model = Article
         fields = '__all__'
-        
         
 class ArticleCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ("title", "image", "content")
-
         
 class ArticleListSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField()
